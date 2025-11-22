0585e0fef9c9617c5b635623cc1d6a57668dfedf
Naveenpl1081 <123857457+Naveenpl1081@users.noreply.github.com>
2025-11-22 12:25:35 +0530

    feat:update the middleware

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -4,6 +4,7 @@ import { AdminController } from "../controllers/adminController";
 import { ProblemController } from "../controllers/problemController";
 import { CategoryController } from "../controllers/categoryController";
 import { SubscriptionPlanController } from "../controllers/subscriptionPlanController";
+import { AuthMiddleware } from "../middleware/authMiddleware";
 
 export class AdminRoutes {
   private router: Router;
@@ -20,6 +21,7 @@ export class AdminRoutes {
     const subscriptionPlanController = container.resolve(
       SubscriptionPlanController
     );
+    const authMiddleware = container.resolve(AuthMiddleware);
 
     this.router.post("/login", adminController.login.bind(adminController));
     this.router.get(
@@ -27,77 +29,77 @@ export class AdminRoutes {
       adminController.getAllUsers.bind(adminController)
     );
     this.router.patch(
-      "/users/:id",
+      "/users/:id",authMiddleware.authenticate,
       adminController.toggleUserStatus.bind(adminController)
     );
     this.router.post("/logout", adminController.logout.bind(adminController));
     this.router.get(
-      "/recruiterlist",
+      "/recruiterlist",authMiddleware.authenticate,
       adminController.getAllRecruiters.bind(adminController)
     );
     this.router.patch(
-      "/recruiter/:id",
+      "/recruiter/:id",authMiddleware.authenticate,
       adminController.toggleRecruiterStatus.bind(adminController)
     );
 
     this.router.get(
-      "/applicantlist",
+      "/applicantlist",authMiddleware.authenticate,
       adminController.getAllApplicants.bind(adminController)
     );
 
     this.router.patch(
-      "/approve-applicant/:applicantId",
+      "/approve-applicant/:applicantId",authMiddleware.authenticate,
       adminController.acceptApplicant.bind(adminController)
     );
     this.router.patch(
-      "/reject-applicant/:applicantId",
+      "/reject-applicant/:applicantId",authMiddleware.authenticate,
       adminController.rejectApplicant.bind(adminController)
     );
     this.router.get(
-      "/problems",
+      "/problems",authMiddleware.authenticate,
       problemController.getAllProblems.bind(problemController)
     );
     this.router.post(
-      "/addproblems",
+      "/addproblems",authMiddleware.authenticate,
       problemController.addProblemController.bind(problemController)
     );
     this.router.put(
-      "/updateproblem/:problemId",
+      "/updateproblem/:problemId",authMiddleware.authenticate,
       problemController.updateProblemController.bind(problemController)
     );
     this.router.post(
-      "/addcategory",
+      "/addcategory",authMiddleware.authenticate,
       categoryController.addCategoryController.bind(categoryController)
     );
     this.router.get(
-      "/allcategory",
+      "/allcategory",authMiddleware.authenticate,
       categoryController.getAllCategories.bind(categoryController)
     );
     this.router.get(
-      "/categorylist",
+      "/categorylist",authMiddleware.authenticate,
       categoryController.categoryList.bind(categoryController)
     );
     this.router.put(
-      "/updatecategory/:categoryId",
+      "/updatecategory/:categoryId",authMiddleware.authenticate,
       categoryController.updateCategoryController.bind(categoryController)
     );
 
     this.router.patch(
-      "/category/:id",
+      "/category/:id",authMiddleware.authenticate,
       categoryController.toggleCategoryStatus.bind(categoryController)
     );
     this.router.patch(
-      "/problems/:id",
+      "/problems/:id",authMiddleware.authenticate,
       problemController.toggleProblemStatus.bind(problemController)
     );
     this.router.post(
-      "/addsubscription",
+      "/addsubscription",authMiddleware.authenticate,
       subscriptionPlanController.addSubscriptionPlan.bind(
         subscriptionPlanController
       )
     );
     this.router.get(
-      "/subscriptions",
+      "/subscriptions",authMiddleware.authenticate,
       subscriptionPlanController.getAllSubscriptions.bind(
         subscriptionPlanController
       )
